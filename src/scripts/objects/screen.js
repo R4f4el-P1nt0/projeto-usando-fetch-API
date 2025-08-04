96c92f26d311d04d236117880833f8279b20939d
Rafael Pinto de Sousa <rafaelpdesousa27@gmail.com>
2025-08-04 20:18:28 -0300

    Add files via upload

--- src/scripts/objects/screen.js
+++ src/scripts/objects/screen.js
@@ -1,79 +1,27 @@
 const screen = {
-    userProfile: document.querySelector(".profile-data"),
-    renderUser(user){
+    userProfile: document.querySelector('.profile-data'),
+    renderUser(user) {
         this.userProfile.innerHTML = `<div class="info">
-                                            <img src="${user.avatarUrl}" alt="Foto do perfil do usuário />"
-                                            <div class="data"> 
-                                                <div class="infos">
-                                                    <h1>${user.name ?? "Não possui nome cadastrado 😓"}</h1>
-                                                    <p>${user.bio ?? "Não possui bio cadastrada 😓"}</p>
-                                                    <p>&#x1F465; Seguidores: ${user.followers}</p>
-                                                    <p>&#x1F464; Seguindo: ${user.following}</p>
-                                                </div>    
-                                            </div>
-                                      </div>`
-        
-        let repositoriesItens = ''
-        user.repositories.forEach(repo => repositoriesItens += `<li><a href="${repo.html_url}" target="_blank">${repo.name} <br>
-                                                                        <div class="languages">
-                                                                            <p class="estilo">
-                                                                                🍴${repo.forks}
-                                                                            </p>    
-                                                                                
-                                                                            <p class="estilo">
-                                                                            🌟${repo.stargazers_count}
-                                                                            </p>
-                                                                            
-                                                                            <p class="estilo">
-                                                                            👀${repo.watchers}
-                                                                            </p>
+                            <img src="${user.avatarUrl}" alt="Foto do perfil do usuário" />
+                            <div class="data">
+                            <h1>${user.name ?? 'Não possui nome cadastrado 😢'}</h1>
+                            <p>${user.bio ?? 'Não possui nome cadastrado 😢'}</p>
+                            </div>
+                        </div>`
 
-                                                                            <p class="estilo">
-                                                                            👨‍💻${repo.language ?? "Não possui linguagem"}
-                                                                            </p>
-                                                                        </div> 
-                                                                            </a></li>`)
+        let repositoriesItems = '';
+        user.repositories.forEach(repo => repositoriesItems += `<li><a href="${repo.html_url}" target="_blank">${repo.name}</a></li>`);
 
-        if(user.repositories.length > 0){
+        if (user.repositories.length > 0) {
             this.userProfile.innerHTML += `<div class="repositories section">
-                                               <h2>Repositórios</h2>
-                                               <ul>${repositoriesItens}</ul>
-                                           </div>`
-        }
-
-      
-        let eventsList = user.events.filter(
-            event => event.type === "PushEvent" || event.type === "CreateEvent"
-          );
-            
-          let lastTenEvents = eventsList.splice(0, 10);
-          let displayEvents = "";
-      
-        lastTenEvents.forEach(event => {
-            let eventName = '';
-            let messageEvent = '';
-            if(event.type === "PushEvent"){
-                eventName = event.repo.name;
-                messageEvent = event.payload.commits[0].message;
-            } else if(event.type === "CreateEvent"){
-                eventName = event.repo.name;
-                messageEvent = "Não possui commits"
-            } else {
-                return;
+                                            <h2>Repositórios</h2>
+                                            <ul>${repositoriesItems}</ul>
+                                        </div>`;
             }
-
-            displayEvents += `<li><a href="https://github.com/${eventName}" target="_blank">${eventName}</a> - ${messageEvent}</li>`
-
-        });
-
-        this.userProfile.innerHTML += `<div class="events">
-                                          <h2 class="tittle-events">Eventos</h2>
-                                          <ul>${displayEvents}</ul>
-                                       </div>`
-    },
-    renderNotFound(){
-        this.userProfile.innerHTML = "Usuário não encontrado"
+        },
+        renderNotFound() {
+            this.userProfile.innerHTML = `<h3>Usuário não encontrado 😢</h3>`;
+        }
     }
-} 
 
-export { screen }
+export { screen };
--- src/scripts/objects/user.js
+++ src/scripts/objects/user.js
@@ -2,25 +2,18 @@ const user = {
     avatarUrl: '',
     name: '',
     bio: '',
-    userName: '',
-    followers: '',
-    following: '',
+    username: '',
     repositories: [],
-    events: [],
-    setInfo(gitHubUser){
-        this.avatarUrl = gitHubUser.avatar_url
-        this.name = gitHubUser.name
-        this.bio = gitHubUser.bio
-        this.userName = gitHubUser.login
-        this.followers = gitHubUser.followers
-        this.following = gitHubUser.following
+    setInfo(githubUser) {
+        this.avatarUrl = githubUser.avatar_url;
+        this.name = githubUser.name;
+        this.bio = githubUser.bio;
+        this.username = githubUser.login;
     },
-    setRepositories(repositories){
-        this.repositories = repositories
-    },
-    setEvents(events){
-        this.events = events
+
+    setRepositories(repositories) {
+        this.repositories = repositories;
     }
 }
 
-export { user }
+export { user };
